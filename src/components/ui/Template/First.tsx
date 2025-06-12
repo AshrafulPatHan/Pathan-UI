cefefc8163962313cb0ac00ca5bb605dfa3ab75c
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-06-12 12:21:24 +0600

    add boiler plate code

--- src/components/ui/Template/First.tsx
+++ src/components/ui/Template/First.tsx
@@ -1,11 +1,182 @@
 import React from 'react';
+import Desktop from '../../../assets/UI/Background.svg';
+
+
 
 const First = () => {
     return (
-        <div>
-            <main>
-                
+        <div className='pt-24 pb-24 '
+            style={{
+                backgroundImage: `url(${Desktop})`,
+                backgroundSize: 'cover',
+                // backgroundPosition: 'center',
+                height: '100vh'
+            }}>
+            <main > 
+                {/* left side section */}
+                <div>
+                    <button>Bay at 50% off</button>
+                    <h2></h2>
+                    <p></p>
+                    <div>
+                        <div>
+                            <button></button>  
+                            <button></button>  
+                        </div>
+                        <button></button>
+                        <button></button>
+                    </div>
+                </div>
+                {/* the right side section */}
+                <div>
+                    {/* section 1 */}
+                    <div>
+                        {/* mode dark and withe */}
+                        <div>
+                            <button></button>
+                            <button></button>
+                            <button></button>
+                        </div>
+                        {/* card 1 */}
+                        <div>
+                            <div>
+                                <img src="" alt="" />
+                            </div>
+                            <div>
+                                <div>
+                                    <p>New Update</p>
+                                    <p></p>
+                                </div>
+                                <div>
+                                    <button></button>
+                                    <button></button>
+                                </div>
+                            </div>
+                            <div>
+                                <h3></h3>
+                                <p></p>
+                                <button>Browse template</button>
+                            </div>
+                        </div>
+                        {/* card 2 */}
+                        <div>
+                            <div>
+                                <div>
+                                    <p></p>
+                                    <p></p>
+                                    <p></p>
+                                </div>
+                                <div></div>
+                                <div></div>
+                                <div>
+                                    <button></button>
+                                    <button></button>
+                                </div>
+                                <div>
+                                    <button></button>
+                                    <button></button>
+                                    <button></button>
+                                </div>
+                                <div>
+                                    <button></button>
+                                    <button></button>
+                                    <button></button>
+                                    <button></button>
+                                </div>
+                                <div>
+                                    <button></button>
+                                    <button></button>
+                                    <button></button>
+                                </div>
+                            </div>
+                            <div>
+                                <button>Monthly</button>
+                                <button>yearly</button>
+                            </div>
+                        </div>
+                        {/* card 3 */}
+                        <div>
+                            <div>
+                                <img src="" alt="" />
+                            </div>
+                            <div>
+                                <p></p>
+                                <p></p>
+                            </div>
+                            <h3>Smart card</h3>
+                            <p></p>
+                            <div>
+                                <button></button>
+                                <div>
+                                    <button></button>
+                                    <button></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* section 2 */}
+                    <div>
+                        {/* card 1 */}
+                        <div>
+                            <div>
+                                <button>choose</button>
+                                <button>menu</button>
+                            </div>
+                            <div>
+                                <div>Account</div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div>Sign out</div>
+                            </div>
+                            <div>
+                                <input type="text" name="" id="" />
+                            </div>
+                        </div>
+                        {/* card 2 */}
+                        <div>
+                            <div>
+                                <button>Email address</button>
+                                <button>Subscribe</button>
+                            </div>
+                            <div>
+                                <div>
+                                    <p>Notice</p>
+                                    <div>
+                                        <p></p>
+                                        <p></p>
+                                        <div>
+                                            <img src="" alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div>
+                                    <p></p>
+                                    <button>Mark all as read</button>
+                                </div>
+                            </div>
+                        </div>
+                        {/* card 3 */}
+                        <div>
+                            <p>All-Access</p>
+                            <div>
+                                <p>$</p>
+                                <h3>49</h3>
+                                <sub>$ 99</sub>
+                            </div>
+                            <p></p>
+                            <div></div>
+                            <button></button>
+                        </div>
+                   </div>
+                </div>
             </main>
+            <code></code>
         </div>
     );
 };
