e55600b2754b155d3fbaaf5332df4d8a94750b62
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-11-05 13:55:11 +0600

    init footer

--- src/components/navigation/footer/Footer.tsx
+++ src/components/navigation/footer/Footer.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import Drawer from "../../ui/Drawer";
+import NormalFooter from "./NormalFot";
+
 
 const Footer = () => {
     return (
         <div>
+            <Drawer/>
+            <div>
+                <div>
+                    <NormalFooter/>
+                </div>
+            </div>
             
         </div>
     );
