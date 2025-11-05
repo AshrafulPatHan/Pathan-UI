f343e94ca61a22ba9840bad107181bdc14531e33
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-11-05 13:04:16 +0600

    add some card and start the navbar

--- src/components/navigation/navbar/NormalNav.tsx
+++ src/components/navigation/navbar/NormalNav.tsx
@@ -4,59 +4,20 @@ import { IoCopyOutline } from "react-icons/io5";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import ImageS from "../../../assets/image/astronot.jpg"
 
-const NormalCard = () => {
+const NormalNav = () => {
     // 🔹 JSON Data (UI preview + code in one place)
     const componentsData = [
         {
-            title: "Basic Card",
+            title: "Basic Navbar",
             code: `
-# card 1
-<div className='w-80 '>
-    <div className='w-80 h-auto overflow-hidden object-center rounded-lg'>
-        <img src={ImageS} alt="photo" className='w-full h-full object-cover scale-100 hover:scale-110 transition-transform duration-300 ease-in-out ' />
-    </div>
-    <div className='text-sm font-mono'>
-        <h2 className='text-lg font-bold font-serif mt-2'>Title</h2>
-        <p className='mb-1 font-semibold'>Subtitle of the card</p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id maiores ex facilis explicabo voluptates fuga pariatur, atque quisquam! Sed!
-        </p>
-    </div>
+<div>
+
 </div>
-# card 2
 
             `,
             preview: [
-                // card 1
-                <div className='w-80 '>
-                    <div className='w-80 h-auto overflow-hidden object-center rounded-lg'>
-                        <img src={ImageS} alt="photo" className='w-full h-full object-cover scale-100 hover:scale-110 transition-transform duration-300 ease-in-out ' />
-                    </div>
-                    <div className='text-sm font-mono'>
-                        <h2 className='text-lg font-bold font-serif mt-2'>Title</h2>
-                        <p className='mb-1 font-semibold'>Subtitle of the card</p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id maiores ex facilis explicabo voluptates fuga pariatur, atque quisquam! Sed!
-                        </p>
-                    </div>
-                </div>,
-                // card 2
-                <div className='w-80 '>
-                    <div className='w-80 h-auto overflow-hidden object-center rounded-lg'>
-                        <img src={ImageS} alt="photo" className='w-full h-full object-cover scale-100 hover:scale-110 transition-transform duration-300 ease-in-out ' />
-                    </div>
-                    <div className='text-sm font-mono'>
-                        <div className='flex items-center justify-between'>
-                            <h2 className='text-lg font-bold font-serif mt-2'>Title</h2>
-                            <button className='mr-1'>
-                                <FaRegHeart className='text-xl'/>
-                            </button>
-                        </div>
-                        <p className='mb-1 font-semibold'>Subtitle of the card</p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus id maiores ex facilis explicabo voluptates fuga pariatur, atque quisquam! Sed!
-                        </p>
-                    </div>
+                <div>
+
                 </div>
             ]
         },
@@ -80,7 +41,7 @@ const NormalCard = () => {
         };
 
         return (
-            <div className="flex flex-col gap-5 p-7 rounded-2xl shadow-xl bg-white w-[90%] lg:w-[60%] max-w-5xl">
+            <div className="flex flex-col gap-5 p-7 rounded-2xl shadow-xl bg-white w-[90%]">
                 {/* Header */}
                 <div className="flex items-center justify-between">
                     <h2 className="text-2xl font-bold">{title}</h2>
@@ -150,4 +111,4 @@ const NormalCard = () => {
     );
 };
 
-export default NormalCard;
+export default NormalNav;
