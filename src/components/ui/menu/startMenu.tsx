b3f695fafc52c65c01b1141fdb4a5c6165883363
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-12-25 14:01:26 +0600

    fix all isue

--- src/components/ui/menu/startMenu.tsx
+++ src/components/ui/menu/startMenu.tsx
@@ -9,12 +9,12 @@ const StartMenu = () => {
     const [open, setOpen] = useState(false);
 
     // dropdown menu
-    const dropdownRef = useRef(null);
+    const dropdownRef =  useRef<HTMLDivElement | null>(null);
 
     // after clikin button dropdown off
     useEffect(() => {
-        const handleClickOutside = (e: { target: unknown; }) => {
-            if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
                 setOpen(false);
             }
         };
